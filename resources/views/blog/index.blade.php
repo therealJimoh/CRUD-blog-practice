9c2f8e2cd9d88b027b1f07fcf14ef2061289c1b7
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2021-04-26 10:18:02 +0100

    create and store posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,29 +9,29 @@
     </div>
 </div>
 
-{{ $posts }}
-
+@foreach ($posts as $post)
 <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
     <div>
         <img src="https://cdn.pixabay.com/photo/2015/02/02/11/09/office-620822__180.jpg" width="700" alt="">
     </div>
     <div>
         <h2 class="text-gray-700 font-bold text-5xl pb-4">
-            Learn how to write Laravel code.
+            {{ $post->title }}
         </h2>
 
         <span class="text-gray-500">
-            By <span class="font-bold italic text-gray-800">The Author</span>, 1 day ago
+            By <span class="font-bold italic text-gray-800"> {{ $post->user }} </span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
         </span>
 
         <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos.
+            {{ $post->description }}
         </p>
 
-        <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+        <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
             Keep Reading
         </a>
     </div>
 </div>
+@endforeach
 
 @endsection
